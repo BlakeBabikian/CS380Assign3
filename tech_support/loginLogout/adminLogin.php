da48f505ab4b6e15097b5aff389c8c429417ce74
blakebabikianbentley <blakebabikian@gmail.com>
2023-11-29 13:41:22 -0500

    Blake - found it

---
+++ tech_support/loginLogout/adminLogin.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin Login</title>
+    <meta name="description" content="Your page description">
+    <meta name="keywords" content="keywords, for, your, page">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="../css/main.css">
+    <link rel="icon" type="image/ico" href="../images/favicon.ico">
+</head>
+<body>
+<?php include '../view/header.php'; ?>
+<?php
+error_reporting(0);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+$con = null;
+$username = null;
+require '../model/database.php';
+require '../errors/testInput.php';
+session_start();
+if (($_SESSION["ValidAdmin"])) header("Location: ../homePages/adminHome.php");
+
+elseif (! empty($_POST['Login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $test1 = test_input($username);
+    $test2 = test_input($password);
+    if ($test1 === true && $test2 === true) {
+        try {
+            $query = mysqli_prepare($con, "SELECT * FROM administrators WHERE username=? AND password=?");
+            mysqli_stmt_bind_param($query, "ss", $username, $password);
+            mysqli_stmt_execute($query);
+            $result = mysqli_stmt_get_result($query);
+            $line = mysqli_fetch_array($result, MYSQLI_ASSOC); # get array based of query
+            if (!empty($line['username'])) {
+                $username = $_SESSION['username'] = $line['username']; # save customer first name for display
+                $password = $_SESSION['password'] = $line['password']; # save customer last name for display
+                $_SESSION['ValidAdmin'] = true;
+                header("Location: ../homePages/adminHome.php");}
+        }
+        catch (Exception $e) {
+            $error = 'Did not recognize customer email.'.$e; # message
+            header("Location: ../errors/database_error.php?error_message=$error");}
+    }
+    else header("Location: ../errors/error.php?error=$test1");
+}
+?>
+<main id="aligned">
+    <h1>Admin Login</h1>
+    <form action="adminLogin.php" method="post">
+        <label for="username">Username: </label>
+        <input type="text" id='username' name='username'><br>
+        <label for="password">Password: </label>
+        <input type="password" id='password' name='password'><br>
+        <input type="submit" id="login" name="Login" style="margin-left: 145px"><br>
+    </form>
+</main>
+<?php include '../view/footer.php'; ?>
+</body>
+</html>
+
---
+++ tech_support/loginLogout/technicianLogin.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Technician Login</title>
+    <meta name="description" content="Your page description">
+    <meta name="keywords" content="keywords, for, your, page">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="../css/main.css">
+    <link rel="icon" type="image/ico" href="../images/favicon.ico">
+</head>
+<body>
+<?php include '../view/header.php'; ?>
+<?php
+error_reporting(0);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+$con = null;
+$email = null;
+require '../model/database.php';
+require '../errors/testInput.php';
+session_start();
+if (($_SESSION["ValidTech"])) header("Location: ../homePages/techHome.php");
+
+elseif (! empty($_POST['Login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $test1 = test_input($email);
+    $test2 = test_input($password);
+    if ($test1 === true && $test2 === true) {
+        try {
+            $query = mysqli_prepare($con, "SELECT * FROM technicians WHERE email=? AND password=?");
+            mysqli_stmt_bind_param($query, "ss", $email, $password);
+            mysqli_stmt_execute($query);
+            $result = mysqli_stmt_get_result($query);
+            $line = mysqli_fetch_array($result, MYSQLI_ASSOC); # get array based of query
+            if (!empty($line['firstName'])) {
+                $first_name = $_SESSION['firstName'] = $line['firstName']; # save customer first name for display
+                $last_name = $_SESSION['lastName'] = $line['lastName']; # save customer last name for display
+                $id = $_SESSION['techID'] = $line['techID']; # save customer ID for later data entry
+                $_SESSION['Email'] = $email;
+                $_SESSION['ValidTech'] = true;
+                header("Location: ../homePages/techHome.php");}
+        }
+        catch (Exception $e) {
+            $error = 'Did not recognize customer email.'.$e; # message
+            header("Location: ../errors/database_error.php?error_message=$error");}
+    }
+    else header("Location: ../errors/error.php?error=$test1");
+}
+?>
+<main id="aligned">
+    <h1>Technician Login</h1>
+    <form action="technicianLogin.php" method="post">
+        <label for="email">email: </label>
+        <input type="text" id='email' name='email'><br>
+        <label for="password">Password: </label>
+        <input type="password" id='password' name='password'><br>
+        <input type="submit" id="Login" name="Login" style="margin-left: 145px"><br>
+    </form>
+</main>
+<?php include '../view/footer.php'; ?>
+</body>
+</html>
+
+
